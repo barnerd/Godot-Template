81185cf7da507e626c39c27141e6c386ce71350a
barnerd <dibarner@gmail.com>
2024-06-04 20:37:45 -0700

    Update signal_bus.gd
    cleanup

--- ProjectTemplate/SignalBus/signal_bus.gd
+++ ProjectTemplate/SignalBus/signal_bus.gd
@@ -3,9 +3,9 @@ extends Node
 var signal_list: Dictionary = {} # String -> Signal
 
 
-func get_signal(_name: String) -> Signal:
-	if signal_list.has(_name):
-		return signal_list[_name]
+func get_signal(name: String) -> Signal:
+	if signal_list.has(name):
+		return signal_list[name]
 	else:
 		# return empty signal. similar to null, but signal isn't nullable
 		# check for null by using:
@@ -13,18 +13,18 @@ func get_signal(_name: String) -> Signal:
 		return Signal()
 
 
-func register_signal(_name: String, _signal: Signal):
-	if not signal_list.has(_name):
-		signal_list[_name] = _signal
+func register_signal(name: String, _signal: Signal):
+	if not signal_list.has(name):
+		signal_list[name] = _signal
 	else:
-		print("%s already registered" % _name)
+		print("%s already registered" % name)
 
 
-func connect_to_signal(_name: String, _callable: Callable):
-	if signal_list.has(_name):
-		if not signal_list[_name].is_connected(_callable):
-			signal_list[_name].connect(_callable)
+func connect_to_signal(name: String, callable: Callable):
+	if signal_list.has(name):
+		if not signal_list[name].is_connected(callable):
+			signal_list[name].connect(callable)
 		else:
-			print("%s is already connected to %s" % [_callable, _name])
+			print("%s is already connected to %s" % [callable, name])
 	else:
-		print("%s not found" % _name)
+		print("%s not found" % name)
